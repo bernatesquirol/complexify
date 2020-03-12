58eb916211daf5c01f0a286a793872c566367396
Bernat <bernatesquirol@gmail.com>
2020-03-12 17:42:09 +0100

    create_graph prune function & pruned graphs

--- create_graph.py
+++ create_graph.py
@@ -9,7 +9,7 @@ songs['artist']=songs['artists'].apply(lambda x: x[0][1])
 # songs[songs['name'].apply(lambda x: 'where is the love' in x.lower())]
 # -
 
-len(songs['artist'].unique())
+# # Creating the graph
 
 import networkx as nx
 from networkx.algorithms import bipartite
@@ -31,17 +31,53 @@ for artist, group in songs[['artist','playlists']].groupby('artist'):
         for playlist in playlists:
             spotify.add_edge(artist, playlist)
 
-artists_playlist = pd.Series(dict_artists)
-
-top_nodes = {n for n, d in spotify.nodes(data=True) if d['bipartite']==0}
-bottom_nodes = set(spotify) - top_nodes
-
-len(top_nodes)
-
-spotify
 
 spotify.remove_nodes_from(list(nx.isolates(spotify)))
 
-nx.readwrite.write_gpickle(spotify, 'data/network_bipartite.gpkl')
+# +
+# nx.readwrite.write_gpickle(spotify, 'data/network_bipartite.gpkl')
+# -
+
+# # Adding features to the graph
+
+# +
+# import pandas as pd
+# import numpy as np
+# import networkx as nx
+
+# spotify = nx.readwrite.read_gpickle('data/network_bipartite.gpkl')
 
 
+# +
+# artists = dict(np.vstack(songs.artists.values))
+# artists = pd.Series({v: k for k, v in artists.items()})
+
+# +
+# dict_labels = {}
+# for n, d in spotify.nodes(data=True):
+#     print(n)
+#     if d['bipartite']==0:
+#         dict_labels[n] = playlists[n]
+#     else:
+#         dict_labels[n] = artists[n]
+
+
+# +
+# nx.get_node_attributes(spotify, '37i9dQZF1DWT8aqnwgRt92')
+# -
+
+# # Proning the graph
+
+# +
+# spotify = nx.read_gpickle('data/network_bipartite.gpkl')
+# -
+
+def prune(spotify, playlists):
+    spotify_small = spotify.copy()
+    playlists_articulation = [i for i in nx.articulation_points(spotify) if i in playlists.index]
+    spotify_small.remove_nodes_from(playlists_articulation)
+    stay_nodes = list(nx.connected_components(spotify_small))[np.argmax([len(i) for i in nx.connected_components(spotify_small)])]
+    return spotify_small.subgraph(stay_nodes)
+
+# +
+# G = prone(spotify, playlists)
